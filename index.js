c461efe143e42e9c7a5aef22480c0f74ba0f98fd
Cameron Fraser <cfraser497@gmail.com>
2023-07-25 18:57:11 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import 'dotenv/config';
+import axios from 'axios';
+
+import setAuthToken from './lib/setAuthToken.js';
+import getPlaylistItems from './lib/getPlaylistItems.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//get and set auth token for session
+if (!axios.defaults.headers.common['Authorization']) {
+    await setAuthToken();
+    console.log("auth token set");
+}
+
+const songIds = await getPlaylistItemsIds(process.env.PLAYLIST_ID);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'))
